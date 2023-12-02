9356b4e46a9cf20eb6ea71d2443179be8568c0ae
mihailmarkevich <mihail.markevich.nox@gmail.com>
2023-12-02 21:51:30 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿using System.Windows.Forms;
+
+namespace Notificator.GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            panel1 = new Panel();
+            buttonCancel = new Button();
+            buttonSave = new Button();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            richTextBox1 = new RichTextBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            buttonNewEvent = new Button();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 60);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(207, 379);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(checkBox1);
+            panel1.Controls.Add(buttonCancel);
+            panel1.Controls.Add(buttonSave);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(richTextBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(225, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(563, 427);
+            panel1.TabIndex = 2;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Enabled = false;
+            buttonCancel.Location = new Point(133, 386);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(75, 23);
+            buttonCancel.TabIndex = 7;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            buttonCancel.Click += buttonCancel_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Enabled = false;
+            buttonSave.Location = new Point(24, 386);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(103, 23);
+            buttonSave.TabIndex = 6;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 222);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Date";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Enabled = false;
+            dateTimePicker1.Location = new Point(24, 240);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(233, 23);
+            dateTimePicker1.TabIndex = 4;
+            dateTimePicker1.Value = DateTime.Now;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Enabled = false;
+            richTextBox1.Location = new Point(24, 98);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(520, 96);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Description";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(24, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(520, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // buttonNewEvent
+            // 
+            buttonNewEvent.Location = new Point(12, 12);
+            buttonNewEvent.Name = "buttonNewEvent";
+            buttonNewEvent.Size = new Size(207, 32);
+            buttonNewEvent.TabIndex = 0;
+            buttonNewEvent.Text = "Create Event";
+            buttonNewEvent.UseVisualStyleBackColor = true;
+            buttonNewEvent.Click += buttonNewEvent_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(24, 288);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Completed";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonNewEvent);
+            Controls.Add(panel1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Notificator";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ListBox listBox1;
+        private Panel panel1;
+        private Button buttonNewEvent;
+        private TextBox textBox1;
+        private Label label1;
+        private Button buttonCancel;
+        private Button buttonSave;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private RichTextBox richTextBox1;
+        private Label label2;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+using System.Data.SqlClient;
+using Notificator.SharedServices;
+using System.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Windows.Forms;
+using Notificator.SharedServices.Models;
+
+namespace Notificator.GUI
+{
+    public partial class Form1 : Form
+    {
+
+        private IEventRepository _eventRepository;
+
+        public List<EventModel> events = null;
+
+        public Form1()
+        {
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            _eventRepository = new EventRepository(connectionString);
+
+            InitializeComponent();
+
+            this.PopulateListBox();
+        }
+
+        /// <summary>
+        ///  Populates the list box with events from the repository.
+        /// </summary>
+        private void PopulateListBox()
+        {
+            this.events = _eventRepository.GetAll().ToList();
+            listBox1.DisplayMember = "Name";
+            listBox1.DataSource = events;
+        }
+
+        /// <summary>
+        ///  Handles the selection of an item in the list box.
+        /// </summary>
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                EventModel selectedEvent = (EventModel)listBox1.SelectedItem;
+
+                textBox1.Text = selectedEvent.Name;
+                richTextBox1.Text = selectedEvent.Description;
+                dateTimePicker1.Value = selectedEvent.Date;
+                checkBox1.Checked = selectedEvent.Status;
+
+                this.PanelStatus(true);
+            }
+        }
+
+        /// <summary>
+        ///  Handles the click event of the "Save" button.
+        /// </summary>
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Update event
+                if (listBox1.SelectedItem != null)
+                {
+                    EventModel selectedEvent = (EventModel)listBox1.SelectedItem;
+                    selectedEvent.Name = textBox1.Text;
+                    selectedEvent.Description = richTextBox1.Text;
+                    selectedEvent.Date = dateTimePicker1.Value;
+                    selectedEvent.Status = checkBox1.Checked;
+
+                    await Task.Run(() =>
+                    {
+                        _eventRepository.Update(selectedEvent);
+                    });
+                }
+                //Create new event
+                else
+                {
+                    EventModel newEvent = new EventModel()
+                    {
+                        Name = textBox1.Text,
+                        Description = richTextBox1.Text,
+                        Date = dateTimePicker1.Value,
+                        Status = checkBox1.Checked,
+                    };
+
+                    await Task.Run(() =>
+                    {
+                        _eventRepository.Create(newEvent);
+                    });
+
+                    this.PanelStatus(false);
+
+                    //Refresh events list
+                    this.PopulateListBox();
+                }
+
+                MessageBox.Show("Operation completed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException !=  null ? ex.InnerException.Message : ex.Message);
+            }
+        }
+
+        /// <summary>
+        ///  Handles the click event of the "New Event" button
+        /// </summary>
+        private void buttonNewEvent_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            checkBox1.Checked = false;
+
+            this.PanelStatus(true);
+        }
+
+        /// <summary>
+        ///  Handles the click event of the "Cancel" button
+        /// </summary>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            checkBox1.Checked = false;
+
+            this.PanelStatus(false);
+        }
+
+        /// <summary>
+        ///  Controls the status (enabled/disabled) of controls within the panel
+        /// </summary>
+        private void PanelStatus(bool status)
+        {
+            foreach (Control control in panel1.Controls)
+            {
+                control.Enabled = status;
+            }
+        }
+
+    }
+}
